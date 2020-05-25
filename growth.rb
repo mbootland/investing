5cbfd595a03808c914c37d1c8f90cd51c39da34e
mbootland <matthew.bootland@gmail.com>
2020-05-25 15:01:34 +0900

    updatd

--- growth.rb
+++ growth.rb
@@ -1,33 +1,21 @@
-# S&P 500 Index
+# frozen_string_literal: true
 
-ten_year_data = [28.61, 6.24, 19.42, 9.54, 0.73, 11.39, 29.60, 13.31, 0.01, 12.78]
+# S&P 500 Index 10 year data
+ten_year_data = [
+  28.61, 6.24, 19.42, 9.54, 0.73, 11.39, 29.60, 13.31, 0.01, 12.78
+]
 ten_year_data_with_dividends = ten_year_data.map{ |x| x = (x += 1.85).round(2) }
 
-# initial_investment_with_compound = 100_000
-#
-# 10.times do |i|
-#   addional_money_per_year = initial_investment_with_compound * (ten_year_data_with_dividends[i] / 100)
-#   puts "An additional $#{addional_money_per_year.round(0)} per year"
-#   initial_investment_with_compound += addional_money_per_year
-# end
-#
-# puts "Initial investment over 10 years with compound growth: $" + initial_investment_with_compound.round.to_s
+puts 'How much is your initial investment in 2010?'
+initial_investment = gets.to_i
 
-initial_investment_with_compound_and_20k = 100_000
+puts 'How much do you want to invest every year?'
+additional_investment = gets.to_i
 
+total_money = initial_investment
 10.times do |i|
-  addional_money_per_year = (initial_investment_with_compound_and_20k * (ten_year_data_with_dividends[i] / 100))
-  initial_investment_with_compound_and_20k += addional_money_per_year
-  initial_investment_with_compound_and_20k += 20_000
+  money_from_growth = total_money * (ten_year_data_with_dividends[i] / 100)
+  total_money += (money_from_growth + additional_investment)
 end
 
-puts "Initial investment over 10 years with compound growth and increased investment of 20k per year: $" + initial_investment_with_compound_and_20k.round.to_s
-
-# initial_investment_without_compound = 50_000
-#
-# 10.times do
-#   initial_investment_without_compound += (50_000 * 0.07)
-#   initial_investment_without_compound += 20_000
-# end
-#
-# puts "Initial investment over 10 years without compound growth and increases investment of 10k: " + initial_investment_without_compound.to_s
+puts 'Your bank account in 2020 would be: ' + total_money.to_s
